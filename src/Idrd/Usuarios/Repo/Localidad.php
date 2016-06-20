635e63e73ac69ec294d8fdee00a7c15c85f6a117
Steven Hernandez Rios <estevenhr@hotmail.com>
2016-06-20 11:24:33 -0500

    Modelo Localidad

---
+++ src/Idrd/Usuarios/Repo/Localidad.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Idrd\Usuarios\Repo;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+use Illuminate\Support\Facades\Config as Config;
+
+class Localidad extends Eloquent {
+	
+	protected $table = 'localidad';
+	protected $primaryKey = 'Id_Localidad';
+	protected $fillable = ['Nombre_Localidad'];
+	protected $connection = '';
+	public $timestamps = false;
+
+	public function __construct()
+	{
+		$this->connection = config('usuarios.conexion');
+	}
+
+
+}
