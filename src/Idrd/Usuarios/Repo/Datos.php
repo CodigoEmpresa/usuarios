b5487701ac09fecbcafeb3cc49a138a494a36fe1
Daniel Feo <danielandres.feo@gmail.com>
2017-01-30 10:16:01 -0500

    Modelo datos ajustes

--- src/Idrd/Usuarios/Repo/Datos.php
+++ src/Idrd/Usuarios/Repo/Datos.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Config as Config;
 class Datos extends Eloquent {
 	
    	protected $table = 'datos';
+    protected $primaryKey = 'Id_Datos';
+     protected $fillable = ['Id_Datos','Id_Persona','Telefono','Email','Celular','Rh','Direccion'];
        
     public function __construct()
     {
